e05e5832a08cff0a40ec7b85cbad8f4af05f40fc
techiemow <mowriyaabk@gmail.com>
2024-07-31 10:23:15 +0530

    Save local changes before pulling

---
+++ Controller/UserDetails.js
@@ -0,0 +1,27 @@
+const { UserModel } = require("../Model/UserSignupmodel")
+
+
+const UserDetails = async(req,res)=>{
+    try{
+        console.log("userId",req.userId)
+        const user = await UserModel.findById(req.userId)
+
+        res.status(200).json({
+            data : user,
+            error : false,
+            success : true,
+            message : "User details"
+        })
+
+        console.log("user",user)
+
+    }catch(err){
+        res.status(400).json({
+            message : err.message || err,
+            error : true,
+            success : false
+        })
+    }
+}
+
+module.exports = UserDetails
--- Routes/Routes.js
+++ Routes/Routes.js
@@ -3,8 +3,10 @@ const Router = express.Router();
 
 const Signup = require("../Controller/SignUp");
 const Login = require("../Controller/Login");
+const UserDetails = require("../Controller/UserDetails");
 
 Router.post("/Signup", Signup);
 Router.get("/Login/:username/:password", Login);
+Router.get("/UserDetails", UserDetails)
 
 module.exports = { Router };
